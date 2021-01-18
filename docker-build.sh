ffa3548c24d5b3e3a15b88409c7743a5b5b9bd1d
Sébastien <sebastien@birondata.com>
2021-01-18 08:23:34 +0100

    enable multi-arch build

--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,8 @@ DEFAULT_TAGS="pharoz/${NAME}:${VERSION}"
 echo -n "TAGS [${DEFAULT_TAGS}]:"
 read TAGS
 for TAG in ${TAGS:-${DEFAULT_TAGS}}; do
-    docker build -t "$TAG" .
+    docker buildx build \
+      --push \
+      --platform linux/amd64,linux/arm/v7,linux/arm64/v8 \
+      -t "$TAG" .
 done
